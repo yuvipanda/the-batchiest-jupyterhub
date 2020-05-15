d673004c95935755452f3c04c0b9e8ecd5fb3f4a
YuviPanda <yuvipanda@gmail.com>
2020-05-15 13:33:20 +0530

    Add script to install traefik
    We can't ship this with the package as the binary
    size is too large

--- tbjh/__init__.py
+++ tbjh/__init__.py
@@ -1,5 +1,13 @@
+import sys
 import os
+import pathlib
+from jupyterhub_traefik_proxy.install import install_traefik
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+CONDA_DIR = pathlib.Path(os.environ['CONDA_DIR'])
 
+def main():
+    install_traefik(CONDA_DIR / 'bin', f'{sys.platform}-amd64', '1.7.18')
 
+if __name__ == '__main__':
+    main()
