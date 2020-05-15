05756f0305c82f60c7f766364af6dc3841009d9e
YuviPanda <yuvipanda@gmail.com>
2020-05-15 13:41:53 +0530

    Put config files in $CONDA_PREFIX/etc/jupyterhub_config.d
    Most appropriate place I believe

--- tbjh/__init__.py
+++ tbjh/__init__.py
@@ -7,7 +7,10 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 CONDA_DIR = pathlib.Path(os.environ['CONDA_DIR'])
 
 def main():
+    # Traefik binary should be installed
     install_traefik(CONDA_DIR / 'bin', f'{sys.platform}-amd64', '1.7.18')
+    # Subdirectory where we look for config items is created
+    os.makedirs(CONDA_DIR / 'etc/jupyterhub_config.d', exist_ok=True)
 
 if __name__ == '__main__':
     main()
--- tbjh/jupyterhub_config.py
+++ tbjh/jupyterhub_config.py
@@ -1,9 +1,10 @@
 import os
+import pathlib
 from glob import glob
 from jupyterhub_traefik_proxy import TraefikTomlProxy
 
 # We require a version of python installed via conda
-CONDA_DIR = os.environ['CONDA_DIR']
+CONDA_DIR = pathlib.Path(os.environ['CONDA_DIR'])
 
 # Don't kill servers when JupyterHub restarts
 c.JupyterHub.cleanup_servers = False
@@ -13,6 +14,6 @@ c.TraefikTomlProxy.should_start = True
 
 # Load arbitrary .py config files if they exist.
 # This is our escape hatch
-extra_configs = sorted(glob(os.path.join(CONDA_DIR, 'jupyterhub_config.d', '*.py')))
+extra_configs = sorted(glob(os.path.join(CONDA_DIR, 'etc', 'jupyterhub_config.d', '*.py')))
 for ec in extra_configs:
     load_subconfig(ec)
