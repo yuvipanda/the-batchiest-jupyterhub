66532de3e6cd09d688cedf12c5652a881da17752
YuviPanda <yuvipanda@gmail.com>
2020-05-15 12:58:51 +0530

    Add base jupyterhub_config.py file
    Also install jupyterhub-traefik-proxy, we will
    use that instead of configurable-http-proxy

---
+++ hub/jupyterhub_config.py
@@ -0,0 +1,18 @@
+import os
+from glob import glob
+from jupyterhub_traefik_proxy import TraefikTomlProxy
+
+# We require a version of python installed via conda
+CONDA_DIR = os.environ['CONDA_DIR']
+
+# Don't kill servers when JupyterHub restarts
+c.JupyterHub.cleanup_servers = False
+
+c.JupyterHub.proxy_class = TraefikTomlProxy
+c.TraefikTomlProxy.should_start = True
+
+# Load arbitrary .py config files if they exist.
+# This is our escape hatch
+extra_configs = sorted(glob(os.path.join(CONDA_DIR, 'jupyterhub_config.d', '*.py')))
+for ec in extra_configs:
+    load_subconfig(ec)
